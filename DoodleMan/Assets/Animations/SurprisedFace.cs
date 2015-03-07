04d40792888ab30b6950f91d8f436e915fe97e14
Jim Peng <jpeng0823@outlook.com>
2015-03-07 08:09:27 -0700

    Prefab change

---
+++ DoodleMan/Assets/Animations/SurprisedFace.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SurprisedFace : MonoBehaviour {
+
+	public GameObject face;
+	public Sprite headSprite;
+
+	void setSurprisedFace() {
+		face.GetComponent<SpriteRenderer>().sprite = headSprite;
+	}
+}
