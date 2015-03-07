ff82e51b3bda0f7daca4efb5c48a96a582581839
Aaron Russell <russell.aaron94@gmail.com>
2015-03-07 00:22:40 -0700

    Wrote EventTrigger.cs script for handling button presses and lever pulls

---
+++ DoodleMan/Assets/Scripts/GameLogicScripts/EventTrigger.cs
@@ -0,0 +1,36 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class EventTrigger : MonoBehaviour {
+	
+	private int currState;
+	public int maxStates;
+	public GameObject[] toggleObjects, stateObjects;
+	
+	// Use this for initialization
+	void Start () {
+		currState = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		// TODO: Animation on lever pull or button press
+		
+	}
+	
+	private void cycleStates() {
+		
+		foreach (GameObject t in toggleObjects) {
+			t.SendMessageUpdwards("toggleState", SendMessageOptions.DontRequireReceiver);
+		}
+		
+		foreach (GameObject s in stateObjects) {
+			currState++;
+			s.SendMessageUpdwards("setState", currState, SendMessageOptions.DontRequireReceiver);
+		}
+		
+	}
+	
+}
