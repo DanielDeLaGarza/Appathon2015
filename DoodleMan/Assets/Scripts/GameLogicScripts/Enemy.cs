022f3feedc6a9307b6611df481a3b15d21800b3e
Aaron Russell <russell.aaron94@gmail.com>
2015-03-07 07:25:05 -0700

    - Renamed Character.cs to Player.cs - Began work on Enemy.cs

---
+++ DoodleMan/Assets/Scripts/GameLogicScripts/Enemy.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour
+{
+
+	public int health = 1, attackDamage = 1;
+	public float moveSpeed = 2f, applyDamageRadius = 1, detectionRange = 4;
+	protected Animator animator;
+	private Component attackPoint;
+	private bool enemyInRange = false;
+
+	// Use this for initialization
+	void Start ()
+	{
+		animator = GetComponent<Animator> ();
+		attackPoint = transform.Find ("AttackPoint").GetComponent<Component> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (health <= 0) {
+			Die ();
+		}
+	}
+
+	Collider2D ApplyDamage ()
+	{
+
+		// TODO: Change this value to the player's layer
+		int layer_bitmask = 1000;
+
+		Vector2 location = new Vector2 (this.attackPoint.transform.position.x, this.attackPoint.transform.position.y);
+
+		Collider2D[] targets = Physics2D.OverlapCircleAll (location, applyDamageRadius, layer_bitmask);
+
+		// There will only ever be one target: the player
+		return targets [0];
+
+	}
+
+	void ReceiveDamage (int damage)
+	{
+		health -= damage;
+	}
+
+	void Die ()
+	{
+		this.gameObject.SetActive (false);
+		GameObject dieEffect = (GameObject)Instantiate (Resources.Load ("Effects/Die"));
+		// TODO: Death animation
+		Destroy (this.gameObject);
+		Destroy (dieEffect, 2);
+	}
+
+	// Direction < 0 indicates left movement, direction > 0 indicates right movement
+	void Move (byte direction)
+	{
+
+		if (direction < 0) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (-0.2f * moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
+		} else {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (0.2f * moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
+		}
+
+	}
+
+}
--- DoodleMan/Assets/Scripts/GameLogicScripts/EventTrigger.cs
+++ DoodleMan/Assets/Scripts/GameLogicScripts/EventTrigger.cs
@@ -2,31 +2,35 @@
 using UnityEngine;
 using System.Collections;
 
-public class EventTrigger : MonoBehaviour {
+public class EventTrigger : MonoBehaviour
+{
 	
 	private int currState;
 	public GameObject[] toggleObjects, stateObjects;
 	
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		currState = 0;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		// TODO: Animation on lever pull or button press
 	}
 	
-	private void cycleStates() {
+	private void cycleStates ()
+	{
 		
 		foreach (GameObject t in toggleObjects) {
-			t.SendMessageUpwards("ToggleState", SendMessageOptions.DontRequireReceiver);
+			t.SendMessageUpwards ("ToggleState", SendMessageOptions.DontRequireReceiver);
 		}
 		
 		foreach (GameObject s in stateObjects) {
 			currState++;
 
-			s.SendMessageUpwards("SetState", currState, SendMessageOptions.DontRequireReceiver);
+			s.SendMessageUpwards ("SetState", currState, SendMessageOptions.DontRequireReceiver);
 		}
 		
 	}
