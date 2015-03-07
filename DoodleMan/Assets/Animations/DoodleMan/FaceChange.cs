ddd897b6fa95fe692fc53d3175cc9e2a8a432663
Daniel De La Garza <josedelagarza@u.boisestate.edu>
2015-03-07 00:16:22 -0700

    git ignore and some animations

---
+++ DoodleMan/Assets/Animations/DoodleMan/FaceChange.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceChange : MonoBehaviour {
+
+	public GameObject face;
+
+	void setRandomFace(){
+		int FaceNum = Random.Range (1, 3);
+		face.GetComponent<SpriteRenderer>.sprite = 
+	}
+	void setDefaultFace(){
+
+	}
+}
