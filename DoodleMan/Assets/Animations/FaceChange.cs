7163576beecb129471706c00e89165684daeacfa
Daniel De La Garza <josedelagarza@u.boisestate.edu>
2015-03-07 01:04:12 -0700

    Idle Animation

---
+++ DoodleMan/Assets/Animations/FaceChange.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceChange : MonoBehaviour {
+
+	public GameObject face;
+	public Sprite[] headSprites;
+
+
+	void setRandomFace(){
+		int FaceNum = Random.Range (0,headSprites.Length);
+		face.GetComponent<SpriteRenderer>().sprite = headSprites[FaceNum];
+	}
+	void setDefaultFace(){
+		face.GetComponent<SpriteRenderer>().sprite = headSprites[0];
+	}
+}
