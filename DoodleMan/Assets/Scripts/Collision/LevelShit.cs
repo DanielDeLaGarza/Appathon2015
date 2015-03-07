3c79a2335eb735d7892b49d758b837a60c1e92d8
Matt Taylor <taylor.matt777@gmail.com>
2015-03-07 14:02:53 -0700

    Make a new level

---
+++ DoodleMan/Assets/Scripts/Collision/LevelShit.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelShit : MonoBehaviour {
+	
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.name == "DoodleMan") {
+			Application.LoadLevel ("level2");
+		}
+	}
+
+}
