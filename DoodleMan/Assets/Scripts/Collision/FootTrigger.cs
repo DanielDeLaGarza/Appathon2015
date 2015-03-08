3eb220b31f9fac15806e44dc7ac15ad4c9c0e249
Matt Taylor <taylor.matt777@gmail.com>
2015-03-08 08:40:06 -0600

    no message

--- DoodleMan/Assets/Scripts/Collision/FootTrigger.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class FootTrigger : MonoBehaviour {
-
-	void OnTriggerEnter2D(Collider2D other){
-		this.SendMessageUpwards("NotFalling",SendMessageOptions.DontRequireReceiver);
-	}
-}
